198755b0f1762b72c8f0ca92e8cc3d6241675b18
Emil Kalimullin <emilkalim895@gmail.com>
2025-06-29 22:34:00 +0300

    Добавил конфигурацию админки для юзеров

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
-# Register your models here.
+from .models import User, Subscription
+
+
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    list_display = ("username", "email", "is_active", "is_staff")
+    search_fields = ("username", "email")
+
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "author")
+    search_fields = ("user__username", "author__username")
